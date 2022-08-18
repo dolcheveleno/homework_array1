0b4ae3ea7269088a3a8352433018b5304e04bacc
Olya <dolcheveleno@mail.ru>
2022-08-18 16:13:15 +0300

    zadacha_1.2

---
+++ zadacha_1.2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+Console.WriteLine("Введите количество строк:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[n, m];
+double[] summ = new double[m];
+for (int i = 0; i < n; i++) 
+{
+    for (int j = 0; j < m; j++) 
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < m; i++) 
+{
+    for (int j = 0; j < n; j++) 
+    {
+        summ[i] = summ[i] + array[j, i]; 
+    }
+}
+Console.Write($"Среднее арифметическое элементов в каждом столбце: ");
+for (int i = 0; i < m; i++)
+{
+    Console.Write($" {Math.Round(summ[i] / n,1)}; ");
+}
+Console.WriteLine();
